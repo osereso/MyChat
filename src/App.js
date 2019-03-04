1dc271b4d972e65603c98225492f1ffa47e7e68c
mbp <mbp@MBP-15-srs.local>
2019-03-04 11:14:51 +0100

    Home basic style and funtionalities

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 import { Router, Scene } from 'react-native-router-flux';
 import Home from './components/Home';
 import Chat from './components/Chat';
+//import { Platform } from 'react-native';
 
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Nativeeee!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <Router>
+        <Scene key='root' style={{paddingTop: Platform.OS === 'ios' ? 64 : 54}}>
+          <Scene key='home' component={Home} title='Home'/>
+          <Scene key='chat' component={Chat} title='Chat'/>
+        </Scene>
+      </Router>
     );
   }
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,16 +3,59 @@ import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-nativ
 import { Actions } from 'react-native-router-flux';
 
 class Home extends Component {
+    state = {
+        name: '',
+    };
     render () {
         return (
             <View>
-                <Text>
-                    Hello Home
+                <Text style={styles.title}>
+                    Enter your name: 
                 </Text>
+                <TextInput 
+                style={styles.nameInput} 
+                placeholder = "Gipsz Jakab"
+                onChangeText={(text) => {
+                    this.setState({
+                        name: text,
+                    });
+                }}
+                value={this.state.name}
+                />
+                <TouchableOpacity 
+                    onPress={() => { 
+                        alert(this.state.name)
+                    // navigate to chat screen and pass it the name
+                    }}
+                > 
+                
+                    <Text style={styles.buttonText}>
+                        Next
+                    </Text>
+                </TouchableOpacity>
             </View>
 
         );
     }
 }
+
+const styles = StyleSheet.create({
+    title: {
+        marginTop: 20,
+        marginLeft: 20,
+        fontSize: 20
+    },
+    nameInput: {
+        padding: 10,
+        height: 40,
+        borderWidth: 2,
+        borderColor: 'black',
+        margin: 20,
+    },
+    buttonText: {
+        marginLeft: 20,
+        fontSize: 20,
+    }
+})
 
 export default Home;
